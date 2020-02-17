45f38c6483d5b1ed84aa498e886d0400351cb2f6
dani-j-d <59181296+dani-j-d@users.noreply.github.com>
2020-02-17 13:17:33 -0600

    setup error handling

--- index.js
+++ index.js
@@ -5,25 +5,17 @@ const express = require('express')
 const app = express()
 
 
-app.use((request, response, next) => {
-    console.log(request.headers)
-
-    next()
-})
-
-
-app.use((request, response, next) => {
-    request.chance = Math.random()
-
-    next()
-})
-
 
 app.get('/', (request, response) => {
-    response.json({
-        chance: request.chance
-    })
+    throw new Error('oops')
 })
 
+app.use((err, request, response, next) => {
+    //log the error, via console.log
+
+    console.log(err)
+
+    response.status(500).send('something broke!!!')
+})
 
 app.listen(3000)
