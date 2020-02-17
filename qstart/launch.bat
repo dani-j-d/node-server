822bba8c1d981849d40cabde410362ee7508e4e7
dani-j-d <59181296+dani-j-d@users.noreply.github.com>
2020-02-17 16:58:38 -0600

    Updated launch.bat
    Removed localtunnel.me interfacing

--- qstart/launch.bat
+++ qstart/launch.bat
@@ -1,11 +1,11 @@
+@echo off
+
 ::going to project dir
 cd "C:\Users\Daniel Douglas\Documents\ArchetypeCode\NodeHTTPserver\qstart"
+
+ECHO "Prepare to start node.js server."
+PAUSE
 
 ::launch node 
 start site.bat
 
-::wait
-timeout -t 5
-
-::launch localtunnel.me
-start tunnel.bat
